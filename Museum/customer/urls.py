5a1ed409500c7d96f71dc111cfb98436f09f8d30
praveen4040 <praveenshalu05@gmail.com>
2022-06-18 10:49:35 +0530

    completed login and register page

--- Museum/customer/urls.py
+++ Museum/customer/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('login',views.login,name='login'),
     path('about',views.about,name='about'),
     path('shop',views.shop,name='shop'),
-    path('register',views.register,name='register'),
     path('bookticket',views.bookticket,name='bookticket')
 ]
--- Museum/customer/views.py
+++ Museum/customer/views.py
@@ -3,14 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
-def login(request):
-    return render(request,'login.html')
 def about(request):
     return render(request,'about.html')
 def shop(request):
     return render(request,'shop.html')
-def register(request):
-    return render(request,'register.html')
 def bookticket(request):
     return render(request,'book_ticket.html')    
     
